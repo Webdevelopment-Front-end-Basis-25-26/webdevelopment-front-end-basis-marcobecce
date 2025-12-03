cc80934297fe5e7c240d32dff394e4d591ac14b2
marcobecce <marco.becce@student.ucll.be>
2025-12-03 17:42:27 +0100

    ...

--- Les15/Script15.js
+++ Les15/Script15.js
@@ -2,33 +2,33 @@ function VraagGetal(waarde) {
     let getal;
 
     do {
+
         getal = prompt(waarde);
 
-        if (getal === "" || getal == null || isNaN(getal)) {
-            alert("fout");
-        }
+    }
+    while (getal === "" || getal == null || isNaN(getal))
+    {
+        return Number(getal);
+    }
 
 
-    } while (getal === "" || getal == null || isNaN(getal))
 
-    return Number (getal);
+
+
 
 }
 
 function vergelijkGetallen(getal1, getal2) {
-    if (getal1 > getal2) 
-        {
-            return "Getal 1 is groter";
-        }
-    else if (getal1 < getal2) 
-        {
-            return "Getal 2 is groter";
-        }    
-    else
-        {
-            return "getallen zijn gelijk";
-        }
-   
+    if (getal1 > getal2) {
+        return "Getal 1 is groter";
+    }
+    else if (getal1 < getal2) {
+        return "Getal 2 is groter";
+    }
+    else {
+        return "getallen zijn gelijk";
+    }
+
 }
 
 
@@ -45,19 +45,40 @@ document.getElementById("oefening1").onclick = function () {
 
 
 
-function vraagGetalInBereik(min, max, getal)
-{
-    let waarde;
+function vraagGetalInBereik(min, max, waarde) {
+    let getal;
 
-    do 
-    {
-        waarde = VraagGetal(getal);
+    do {
+        getal = VraagGetal(waarde);
 
     }
+    while (getal > min || getal < max)
+    {
+        return getal;
+
+    }
+}
+
+function genereerTafels(getal)
+{
+    let tekst;
+    for( let i = 1; i<10 ; i++ )
+        {
+            tekst = "";
+            tekst += getal + " x " + i + " = " + (getal*i) + "<br>";
+            
+        }
+        return tekst;
 }
 
 
 document.getElementById("oefening2").onclick = function () 
 {
+
+    const gekozenGetal = vraagGetalInBereik(1, 20, "Geef een getal tussen 1 en 20.");
+    document.getElementById("getal").textContent = gekozenGetal;
+    const resultaat = genereerTafels(gekozenGetal);
+    document.getElementById("resultaat2").innerHTML = resultaat;
+    
 
 }
