150a95943dee2545ce1c294238f7e914a2be4f82
marcobecce <marco.becce@student.ucll.be>
2025-12-17 12:43:05 +0100

    ...

--- Oefening16/script16.js
+++ Oefening16/script16.js
@@ -15,15 +15,15 @@ function Message(text) {
 
 document.getElementById("addIngredient").onclick = function () {
 
-    var input = ingredientInput.value;
+    var input = ingredientInput.value.toLowerCase();
+
 
     if (input === "") {
         Message("Voer een ingrediënt in!");
     }
-    else if(ingredientenArray.includes(input.toLowerCase()))
-        {
-            Message(input + " staat al in de lijst.");
-        }
+    else if (ingredientenArray.includes(input.toLowerCase())) {
+        Message(input + " staat al in de lijst.");
+    }
     else {
         ingredientenArray.push(input);
         Message(input + " toegevoegd!");
@@ -40,4 +40,51 @@ document.getElementById("showIngredients").onclick = function () {
     else {
         Message("<b>" + ingredientenArray.length + " Ingrediënten:" + "</b>" + "</br>" + ingredientenArray.join(", "));
     }
+}
+
+
+
+
+var pizzaArray = []
+
+function MessagePizza(bericht) {
+    document.getElementById("pizzasResultaat").innerHTML = bericht;
+}
+
+
+
+
+document.getElementById("addPizza").onclick = function () {
+    if (document.getElementById("pizzaNaam").value == "" || document.getElementById("pizzaPrijs").value == "" || isNaN(Number(document.getElementById("pizzaAantal").value))
+        || Number(document.getElementById("pizzaPrijs").value) < 0.01 || Number(document.getElementById("pizzaAantal").value) < 1) {
+        MessagePizza("Vul alle velden correct in!")
+    }
+    else {
+        let pizza = {
+            naam: document.getElementById("pizzaNaam").value,
+            prijs: document.getElementById("pizzaPrijs").value,
+            aantal: document.getElementById("pizzaAantal").value
+        };
+
+        pizzaArray.push(pizza);
+        MessagePizza("Pizza " + pizza.naam + " toegevoegd!" )
+
+        document.getElementById("pizzaNaam").value = "";
+        document.getElementById("pizzaPrijs").value = "";
+        document.getElementById("pizzaAantal").value = 1;
+
+
+    }
+
+        document.getElementById("showPizzas").onclick = function()
+        {
+            if(pizzaArray.length === 0 )
+                {
+                    MessagePizza("Nog geen pizza's toegevoegd");
+                }
+            else
+                {
+                    
+                }
+        }
 }
